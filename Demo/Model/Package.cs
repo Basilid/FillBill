d1c91de9691b193c061ae494c1114a4149203b94
Vasiliy <basilakanail@gmail.com>
2021-03-22 09:28:47 +0300

    Start New

--- Demo/Model/Package.cs
+++ Demo/Model/Package.cs
@@ -2,25 +2,21 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using ReactiveUI.Fody.Helpers;
+using PropertyChanged;
 
 namespace Demo
 {
+    [AddINotifyPropertyChangedInterface]
     public class Package
     {
         public PackageStatus Status { get; set; }
 
-        [Reactive]
         public string Number { get; set; }
-
-        [Reactive]
-        public string Comment { get; set; }
 
         public string SMGSNum => Docs.OfType<SMGS>()
             .Select(s => s.Number)
             .FirstOrDefault();
 
-        [Reactive]
         public ObservableCollection<Doc> Docs { get; set; } = new ObservableCollection<Doc>();
     }
 
